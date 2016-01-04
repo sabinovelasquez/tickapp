49ea8e5f134c8bba497c3cac6e3c83877859af15
depointless <sabinovelasquez@gmail.com>
2016-01-04 17:29:21 -0300

    added app, moved landing

--- landing/source/js/all.js
+++
@@ -1,6 +0,0 @@
-//=require vendor/angular/angular.min
-//=require vendor/angular/angular-animate
-//=require vendor/angular/angular-ui-bootstrap-modal
-//=require vendor/ui-bootstrap-tpls-0.14.3.min
-//= require underscore
-//= require_tree ./lib
