eaa59b10c7a8b9228d6e89be7d6e0dae2d668528
depointless <sabinovelasquez@gmail.com>
2016-01-29 18:06:20 -0300

    test admin

--- src/tickapp/premises/admin.py
+++ src/tickapp/premises/admin.py
@@ -1,18 +1,31 @@
 from django.contrib import admin
 
-from .models import Premise, Choice
+from .models import Premise, Premise_Category, Category, Drink
 
-class ChoiceInline(admin.StackedInline):
-    model = Choice
-    extra = 3
+# class ChoiceInline(admin.StackedInline):
+#     model = Category
+
+# class DrinksInline(admin.StackedInline):
+#   model = Drink
+
+class CategoryInline(admin.StackedInline):
+  model = Premise_Category
 
 class PremiseAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['name']}),
-        ('Info:', {'fields':['description', 'code']}),
-        ('Registry information', {'fields': ['pub_date']}),
-    ]
-    inlines = [ChoiceInline]
-    list_display = ('name', 'code', 'pub_date')
+  fieldsets = [
+      (None,               {'fields': ['name']}),
+      ('Info:', {'fields':['description', 'code']}),
+      ('Registry information', {'fields': ['pub_date']}),
+  ]
+  inlines = [CategoryInline]
+  list_display = ('name', 'code', 'pub_date')
+
+# class DrinkAdmin(admin.ModelAdmin):
+#   inlines = [CategoryInline]
+
+
+
 admin.site.register(Premise, PremiseAdmin)
-admin.site.register(Choice)
+admin.site.register(Drink)
+admin.site.register(Category)
+admin.site.register(Premise_Category)
--- src/tickapp/premises/models.py
+++ src/tickapp/premises/models.py
@@ -6,14 +6,28 @@ from django.utils import timezone
 class Premise(models.Model):
   name = models.CharField(max_length=200)
   code = models.CharField(max_length=20)
-  description = models.CharField(max_length=200)
+  description = models.TextField(max_length=200, blank=True)
   pub_date = models.DateTimeField('published')
   def was_published_recently(self):
     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
   def __unicode__(self):
     return "%s -code: %s" % (self.name, self.code)
 
-class Choice(models.Model):
-  question = models.ForeignKey(Premise, on_delete=models.CASCADE)
-  choice_text = models.CharField(max_length=200)
-  votes = models.IntegerField(default=0)
+class Premise_Category(models.Model):
+  premise = models.ForeignKey(Premise, on_delete=models.CASCADE)
+  cat_name = models.CharField(max_length=200)
+  cat_desc = models.TextField(max_length=100, blank=True)
+  def __unicode__(self):
+    return "%s" % (self.cat_name)
+
+class Drink(models.Model):
+  premise = models.ForeignKey(Premise_Category, on_delete=models.CASCADE)
+  drink_name =  models.CharField(max_length=200)
+  drink_price = models.IntegerField(default=0)
+  drink_desc = models.TextField(max_length=100, blank=True)
+  def __unicode__(self):
+    return "%s" % (self.drink_name)
+
+class Category(models.Model):
+  cat_name = models.CharField(max_length=200)
+  cat_desc = models.TextField(max_length=100, blank=True)
